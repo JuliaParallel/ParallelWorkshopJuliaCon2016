8b17c30d879ee50bd0fa7d0d84a5ca1ec4766415
benditlikeranjan <benditlikeranjan@gmail.com>
2016-06-21 14:42:22 +0000

    Add devectorized version to blackscholes demo

--- src/blackscholes.jl
+++ src/blackscholes.jl
@@ -31,6 +31,30 @@ end
 
 # This next parallel rewrite does two simple things:
 # It devectorizes the entire loop. We're now looping through arrays instead of dealing with them in blocks like we did previously. This is devectorized code while the above serial block is vectorized code. 
+
+function blackscholes_devec(sptprice::Float64, 
+				strike::Vector{Float64}, 
+				rate::Float64, 
+				volatility::Float64, 
+				time::Float64)
+    sqt = sqrt(time)
+    put = similar(strike)
+    for i = 1:size(strike, 1)
+        logterm = log10(sptprice / strike[i])
+        powterm = 0.5 * volatility * volatility
+        den = volatility * sqt
+        d1 = (((rate + powterm) * time) + logterm) / den
+        d2 = d1 - den
+        NofXd1 = 0.5 + 0.5 * erf(0.707106781 * d1)
+        NofXd2 = 0.5 + 0.5 * erf(0.707106781 * d2)
+        futureValue = strike[i] * exp(-rate * time)
+        c1 = futureValue * NofXd2
+        call_ = sptprice * NofXd1 - c1
+        put[i] = call_ - futureValue + sptprice
+    end
+    put
+end
+
 # Affixes the `@threads` macro in front of the `for` to tell Julia that this is a multi-threaded block.
 
 function blackscholes_parallel(sptprice::Float64, 
@@ -69,10 +93,14 @@ function run(iterations)
     t1 = toq()
     println("Serial checksum: ", sum(put1))
     tic()
-    put2 = blackscholes_parallel(sptprice, initStrike, rate, volatility, time)
+    put2 = blackscholes_devec(sptprice, initStrike, rate, volatility, time)
     t2 = toq()
     println("Parallel checksum: ", sum(put2))
-    return t1, t2
+    tic()
+    put3 = blackscholes_parallel(sptprice, initStrike, rate, volatility, time)
+    t3 = toq()
+    println("Parallel checksum: ", sum(put3))
+    return t1, t2, t3
 end
 
 function driver()
@@ -80,12 +108,16 @@ function driver()
     tic()
     iterations = 10^6
     blackscholes_serial(0., Float64[], 0., 0., 0.)
+    blackscholes_devec(0., Float64[], 0., 0., 0.)
     blackscholes_parallel(0., Float64[], 0., 0., 0.)
     println("SELFPRIMED ", toq())
-    tserial, tparallel = run(iterations)
+    tserial, tdevec, tparallel = run(iterations)
     println("Time taken for serial = $tserial")
+    println("Time taken for devec = $tdevec")
     println("Time taken for parallel = $tparallel")
     println("Serial rate = ", iterations / tserial, " opts/sec")
+    println("Devec rate = ", iterations / tdevec, " opts/sec")
     println("Parallel rate = ", iterations / tparallel, " opts/sec")
 end
 driver()
+
